da10562447a5c66e62a67aa9fdc8a28942b251a5
Jonas Cordes <j.cordes@hs-mannheim.de>
2020-11-26 12:39:56 +0100

    change version to 2020-11-01, remove MITK from bundle name

--- Applications/M2aia/CPackOptions.cmake
+++ Applications/M2aia/CPackOptions.cmake
@@ -1,14 +1,14 @@
 # Set M2aia specific CPack options
 
-set(CPACK_PACKAGE_EXECUTABLES "MitkM2aia;MITK M2aia")
-set(CPACK_PACKAGE_NAME "MitkM2aia")
+set(CPACK_PACKAGE_EXECUTABLES "MitkM2aia;M2aia")
+set(CPACK_PACKAGE_NAME "M2aia")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "M2aia - Mass spectrometry imaging applications for interactive analysis in MITK.")
 # Major version is the year of release
 set(CPACK_PACKAGE_VERSION_MAJOR "2020")
 # Minor version is the month of release
 set(CPACK_PACKAGE_VERSION_MINOR "11")
-# Patch versioning is not intended to be done
-set(CPACK_PACKAGE_VERSION_PATCH "00")
+# Patch versioning
+set(CPACK_PACKAGE_VERSION_PATCH "01")
 
 # this should result in names like 2011.09, 2012.06, ...
 # version has to be set explicitly to avoid such things as CMake creating the install directory "MITK M2aia 2020.."
