7acc4ef16971a07c751cba9ee5e30470e71bcd5c
Jonas Cordes <j.cordes@hs-mannheim.de>
2022-08-28 01:11:42 +0200

    split muti component images (context menu)

--- Plugins/org.mitk.gui.qt.m2.PeakPicking/files.cmake
+++ Plugins/org.mitk.gui.qt.m2.PeakPicking/files.cmake
@@ -5,6 +5,7 @@ set(SRC_CPP_FILES
 set(INTERNAL_CPP_FILES
   org_mitk_gui_qt_m2_PeakPicking_Activator.cpp
   m2PeakPickingView.cpp
+  QmitkExportComponentAction.cpp
 )
 
 set(UI_FILES
@@ -14,6 +15,7 @@ set(UI_FILES
 set(MOC_H_FILES
   src/internal/org_mitk_gui_qt_m2_PeakPicking_Activator.h
   src/internal/m2PeakPickingView.h
+  src/internal/QmitkExportComponentAction.h
 )
 
 # list of resource files which can be used by the plug-in
---
+++ Plugins/org.mitk.gui.qt.m2.PeakPicking/src/internal/QmitkExportComponentAction.cpp
@@ -0,0 +1,149 @@
+/*============================================================================
+
+The Medical Imaging Interaction Toolkit (MITK)
+
+Copyright (c) German Cancer Research Center (DKFZ)
+All rights reserved.
+
+Use of this source code is governed by a 3-clause BSD license that can be
+found in the LICENSE file.
+
+============================================================================*/
+#include "QmitkExportComponentAction.h"
+
+#include "mitkRenderingManager.h"
+#include <mitkImageReadAccessor.h>
+#include <mitkImageWriteAccessor.h>
+#include <mitkImage.h>
+
+//needed for qApp
+#include <qcoreapplication.h>
+
+QmitkExportComponentAction::QmitkExportComponentAction()
+{
+}
+
+QmitkExportComponentAction::~QmitkExportComponentAction()
+{
+}
+
+void QmitkExportComponentAction::Run(const QList<mitk::DataNode::Pointer> &selectedNodes)
+{
+  for (const auto &referenceNode : selectedNodes)
+  {
+    if (referenceNode.IsNotNull())
+    {
+      mitk::Image *img = dynamic_cast<mitk::Image *>(referenceNode->GetData());
+      if (img)
+      {
+        const auto inputPixelType = img->GetPixelType();
+        const auto nComponents = inputPixelType.GetNumberOfComponents();
+        const auto ioPixelType = inputPixelType.GetPixelType();
+        if (nComponents == 1)
+          continue;
+
+        if (m_DataStorage.IsNull())
+          continue;
+
+        if (ioPixelType == mitk::PixelType::ItkIOPixelType::RGB)
+        {
+          const std::string channelNames = "RGB";
+          for (unsigned int i = 0; i < channelNames.size(); ++i)
+          {
+            auto output = this->ExportComponentImage(img, i);
+            auto newNode = mitk::DataNode::New();
+            newNode->SetData(output);
+            newNode->SetName(referenceNode->GetName() + "_" + channelNames.at(i));
+            m_DataStorage->Add(newNode, referenceNode);
+          }
+        }
+
+        else if (ioPixelType == mitk::PixelType::ItkIOPixelType::RGBA)
+        {
+          const std::string channelNames = "RGBA";
+          for (unsigned int i = 0; i < channelNames.size(); ++i)
+          {
+            auto output = this->ExportComponentImage(img, i);
+            auto newNode = mitk::DataNode::New();
+            newNode->SetData(output);
+            newNode->SetName(referenceNode->GetName() + "_" + channelNames.at(i));
+            m_DataStorage->Add(newNode, referenceNode);
+          }
+        }
+
+        else if (nComponents <= 4)
+        {
+          for (unsigned int i = 0; i < nComponents; ++i)
+          {
+            auto output = this->ExportComponentImage(img, i);
+            auto newNode = mitk::DataNode::New();
+            newNode->SetData(output);
+            newNode->SetName(referenceNode->GetName() + "_" + std::to_string(i));
+            m_DataStorage->Add(newNode, referenceNode);
+          }
+
+        }
+        else{
+             mitk::IntProperty *componentProperty =
+            dynamic_cast<mitk::IntProperty *>(referenceNode->GetProperty("Image.Displayed Component"));
+            const auto currentComponent = componentProperty->GetValue();
+            auto output = this->ExportComponentImage(img, currentComponent);
+            auto newNode = mitk::DataNode::New();
+            newNode->SetData(output);
+            newNode->SetName(referenceNode->GetName() + "_" + std::to_string(currentComponent));
+            m_DataStorage->Add(newNode, referenceNode);
+        }
+      }
+    }
+  }
+}
+
+
+mitk::Image::Pointer QmitkExportComponentAction::ExportComponentImage(const mitk::Image * img, unsigned int component){
+        const auto inputPixelType = img->GetPixelType();
+        const auto nComponents = inputPixelType.GetNumberOfComponents();
+        const auto &sVector = inputPixelType.GetSize();
+        const auto sElement = sVector / nComponents;
+        const auto nPixels =
+          std::accumulate(img->GetDimensions(), img->GetDimensions() + img->GetDimension(), 1, std::multiplies<>());
+        const auto inGeometry = img->GetSlicedGeometry();
+
+        auto output = mitk::Image::New();
+        auto newPixelType = mitk::MakePixelType(inputPixelType, 1);
+        output->Initialize(newPixelType, 3, img->GetDimensions());
+        output->GetSlicedGeometry()->SetSpacing(inGeometry->GetSpacing());
+        output->GetSlicedGeometry()->SetOrigin(inGeometry->GetOrigin());
+        output->GetSlicedGeometry()->SetDirectionVector(inGeometry->GetDirectionVector());
+
+        mitk::ImageReadAccessor acc(img);
+        mitk::ImageWriteAccessor occ(output);
+        char *oData = (char *)occ.GetData();
+        for (char *iData = (char *)acc.GetData(); iData != (char *)acc.GetData() + (sVector * nPixels);
+             iData += sVector)
+        {
+          std::copy(iData + (component * sElement), iData + ((component + 1) * sElement), oData);
+          oData += sElement;
+        }
+
+        return output;
+}
+
+void QmitkExportComponentAction::SetDataStorage(mitk::DataStorage* dataStorage)
+{
+  m_DataStorage = dataStorage;
+}
+
+void QmitkExportComponentAction::SetFunctionality(berry::QtViewPart* /*functionality*/)
+{
+  //not needed
+}
+
+void QmitkExportComponentAction::SetSmoothed(bool)
+{
+  //not needed
+}
+
+void QmitkExportComponentAction::SetDecimated(bool)
+{
+  //not needed
+}
