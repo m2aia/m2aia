dd0e13bc3b5085b5203e1c67a6baaf541b9e585b
Jonas Cordes <j.cordes@hs-mannheim.de>
2022-10-07 09:51:44 +0200

    add new node descriptor example

--- Plugins/org.mitk.gui.qt.m2.Data/src/internal/org_mitk_gui_qt_m2_Data_Activator.cpp
+++ Plugins/org.mitk.gui.qt.m2.Data/src/internal/org_mitk_gui_qt_m2_Data_Activator.cpp
@@ -17,6 +17,10 @@ See LICENSE.txt or https://www.github.com/jtfcordes/m2aia for details.
 #include "org_mitk_gui_qt_m2_Data_Activator.h"
 #include "m2Data.h"
 #include "m2DataTools.h"
+#include <mitkNodePredicateDataType.h>
+#include <mitkNodePredicateAnd.h>
+#include <mitkNodePredicateOr.h>
+#include <QmitkNodeDescriptorManager.h>
 
 namespace mitk
 {
@@ -24,6 +28,20 @@ namespace mitk
   {
     BERRY_REGISTER_EXTENSION_CLASS(m2Data, context)
     BERRY_REGISTER_EXTENSION_CLASS(m2DataTools, context)
+
+    auto descriptorManager = QmitkNodeDescriptorManager::GetInstance();
+    auto a = mitk::NodePredicateDataType::New("SpectrumImageStack");
+    auto b = mitk::NodePredicateDataType::New("ImzMLSpectrumImage");
+    auto c = mitk::NodePredicateOr::New(a, b);
+    auto f = mitk::TNodePredicateDataType<mitk::Image>::New();
+    auto g = mitk::NodePredicateAnd::New(c, f);
+    descriptorManager->AddDescriptor( new QmitkNodeDescriptor(tr("SpectrumImage"), QString(":/Qmitk/LabelSetImage_48.png"), g, this));
+
+
+
+    // Adding "PlanarLine"
+    mitk::NodePredicateDataType::Pointer isPlanarLine = mitk::NodePredicateDataType::New("PlanarLine");
+    descriptorManager->AddDescriptor(new QmitkNodeDescriptor(QObject::tr("PlanarLine"), QString(":/QtWidgetsExt/PlanarLine_48.png"), isPlanarLine, descriptorManager));
   }
 
   void org_mitk_gui_qt_m2_Data_Activator::stop(ctkPluginContext *context) { Q_UNUSED(context) }
