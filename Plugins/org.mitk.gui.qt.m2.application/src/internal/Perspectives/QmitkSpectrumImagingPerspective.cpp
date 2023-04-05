3a14e2e03fc34248d33abbf0a4d7e44cdd911b30
Jonas Cordes <j.cordes@hs-mannheim.de>
2023-04-05 16:02:14 +0200

    update default perspective

--- Plugins/org.mitk.gui.qt.m2.application/src/internal/Perspectives/QmitkSpectrumImagingPerspective.cpp
+++ Plugins/org.mitk.gui.qt.m2.application/src/internal/Perspectives/QmitkSpectrumImagingPerspective.cpp
@@ -24,34 +24,31 @@ void QmitkSpectrumImagingPerspective::CreateInitialLayout(berry::IPageLayout::Po
   /////////////////////////////////////////////////////
   QString editorArea = layout->GetEditorArea();
   
-  layout->AddView("org.mitk.views.m2.Data", berry::IPageLayout::RIGHT, 0.61f, editorArea);
-  auto lo = layout->GetViewLayout("org.mitk.views.m2.Data");
-  lo->SetCloseable(false);
+  auto folder = layout->CreateFolder("right", berry::IPageLayout::RIGHT, 0.75f, editorArea);
+  folder->AddView("org.mitk.views.m2.Data");
+  folder->AddView("org.mitk.views.viewnavigator");
 
-  auto folder = layout->GetFolderForView("org.mitk.views.m2.Data");
-  folder->AddPlaceholder("org.mitk.views.m2.DataTools");
-  folder->AddPlaceholder("org.mitk.views.m2.Reconstruction3D");
-  folder->AddPlaceholder("org.mitk.views.m2.PeakPicking");
-  folder->AddPlaceholder("org.mitk.views.m2.Registration");
-  folder->AddPlaceholder("org.mitk.views.volumevisualization");
-  folder->AddPlaceholder("org.mitk.views.viewnavigatorview");
+  layout->AddStandaloneView("org.mitk.views.m2.Spectrum", false, berry::IPageLayout::BOTTOM, 0.68f, editorArea);
+  
+  folder = layout->CreateFolder("left", berry::IPageLayout::LEFT, 0.21f, editorArea);
+  folder->AddView("org.mitk.views.datamanager");
+  
+  
+  folder = layout->CreateFolder("bottomleft", berry::IPageLayout::BOTTOM, 0.72f, "left");
+  folder->AddView("org.mitk.views.imagenavigator");
+  folder->AddView("org.mitk.views.pixelvalue");
 
-  layout->AddView("org.mitk.views.datamanager", berry::IPageLayout::BOTTOM, 0.61f, "org.mitk.views.m2.Data");
-  lo = layout->GetViewLayout("org.mitk.views.datamanager");
-  lo->SetCloseable(false);
 
-  layout->AddStandaloneView("org.mitk.views.m2.Spectrum", false, berry::IPageLayout::BOTTOM, 0.61f, editorArea);
-  lo = layout->GetViewLayout("org.mitk.views.m2.Spectrum");
+  auto lo = layout->GetViewLayout("org.mitk.views.datamanager");
   lo->SetCloseable(false);
   
-  layout->AddShowViewShortcut("org.mitk.views.m2.Data");
-  layout->AddShowViewShortcut("org.mitk.views.m2.Ions");
-  layout->AddShowViewShortcut("org.mitk.views.m2.Spectrum");
-  layout->AddShowViewShortcut("org.mitk.views.m2.Reconstruction3D");
-  layout->AddShowViewShortcut("org.mitk.views.m2.CombineImages");
-  layout->AddShowViewShortcut("org.mitk.views.m2.ImzMLExport");
-  layout->AddShowViewShortcut("org.mitk.views.m2.PeakPicking");
-  layout->AddShowViewShortcut("org.mitk.views.m2.Registration");
-  layout->AddShowViewShortcut("org.mitk.gui.qt.matchpoint.evaluator");
+  lo = layout->GetViewLayout("org.mitk.views.m2.Data");
+  lo->SetCloseable(false);
+
+  lo = layout->GetViewLayout("org.mitk.views.viewnavigator");
+  lo->SetCloseable(false);
+
+  lo = layout->GetViewLayout("org.mitk.views.m2.Spectrum");
+  lo->SetCloseable(false);
 
 }
