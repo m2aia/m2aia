cb832ae96382c259e4ee067bf273d2779d99cb13
Jonas Cordes <j.cordes@hs-mannheim.de>
2023-12-05 10:26:28 +0100

    Update CPackOptions.cmake

--- Applications/M2aiaWorkbench/CPackOptions.cmake
+++ Applications/M2aiaWorkbench/CPackOptions.cmake
@@ -3,18 +3,20 @@
 set(CPACK_PACKAGE_EXECUTABLES "M2aiaWorkbench;M2aiaWorkbench")
 set(CPACK_PACKAGE_NAME "M2aiaWorkbench")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "M²aia - Mass spectrometry imaging applications for interactive analysis in MITK.")
-# Major version is the year of release
-set(CPACK_PACKAGE_VERSION_MAJOR "2023")
-# Minor version is the month of release
-set(CPACK_PACKAGE_VERSION_MINOR "10")
-# Patch versioning
-set(CPACK_PACKAGE_VERSION_PATCH "00")
+
+if(not DEFINED ENV{M2AIA_PACKAGE_VERSION})
+  set(CPACK_PACKAGE_VERSION_MAJOR "2023") # Major version is the year of release
+  set(CPACK_PACKAGE_VERSION_MINOR "10") # Minor version is the month of release
+  set(CPACK_PACKAGE_VERSION_PATCH "00") # Patch versioning
+else()
+  set(CPACK_PACKAGE_VERSION ENV{M2AIA_PACKAGE_VERSION})
+endif()
+
 set(CPACK_PACKAGE_VENDOR "m2aia")
 
 # this should result in names like 2011.09, 2012.06, ...
 # version has to be set explicitly to avoid such things as CMake creating the install directory "M2aiaWorkbench 2021.."
 set(CPACK_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}")
-#set(CPACK_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}")#.${CPACK_PACKAGE_VERSION_PATCH}")
 
 set(CPACK_PACKAGE_FILE_NAME "M2aia-${CPACK_PACKAGE_VERSION}-${CPACK_PACKAGE_ARCH}")
 
