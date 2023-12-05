37d665cb098bfe3a6c6301d0e76a44a0f73022b4
Jonas Cordes <j.cordes@hs-mannheim.de>
2023-12-05 16:16:11 +0100

    Update CPackOptions.cmake

--- Applications/M2aiaWorkbench/CPackOptions.cmake
+++ Applications/M2aiaWorkbench/CPackOptions.cmake
@@ -10,14 +10,13 @@ else()
   set(CPACK_PACKAGE_VERSION_MAJOR "2023") # Major version is the year of release
   set(CPACK_PACKAGE_VERSION_MINOR "10") # Minor version is the month of release
   set(CPACK_PACKAGE_VERSION_PATCH "00") # Patch versioning
+  set(CPACK_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}")
 endif()
 
 set(CPACK_PACKAGE_VENDOR "m2aia")
 
 # this should result in names like 2011.09, 2012.06, ...
 # version has to be set explicitly to avoid such things as CMake creating the install directory "M2aiaWorkbench 2021.."
-set(CPACK_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}")
-
 set(CPACK_PACKAGE_FILE_NAME "M2aia-${CPACK_PACKAGE_VERSION}-${CPACK_PACKAGE_ARCH}")
 
 set(CPACK_PACKAGE_ICON "${PROJECT_SOURCE_DIR}/Applications/M2aiaWorkbench/icons/icon.png")
