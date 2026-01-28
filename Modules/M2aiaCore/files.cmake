9858cde71d22ca86bd00f2a539fcca14783830d3
Jonas Cordes <j.cordes@hs-mannheim.de>
2026-01-28 10:00:58 +0100

    fix: add m2PlotData header and source files to CMake configuration

--- Modules/M2aiaCore/files.cmake
+++ Modules/M2aiaCore/files.cmake
@@ -24,6 +24,7 @@ set(H_FILES
   include/m2ImzMLSpectrumImageSource.hpp
   include/m2SpectrumContainerImage.h
   include/m2IntervalVector.h
+  include/m2PlotData.h
   include/m2DataNodePredicates.h
   include/signal/m2Baseline.h
   include/signal/m2EstimateFwhm.h
@@ -52,6 +53,7 @@ set(CPP_FILES
   m2SubdivideImage2DFilter.cpp
   m2SpectrumImageDataInteractor.cpp
   m2IntervalVector.cpp
+  m2PlotData.cpp
   m2ShiftMapImageFilter.cpp
   m2MassSpecVisualizationFilter.cpp
   
