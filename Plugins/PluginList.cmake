f7e1aa2079d27ed5f8855bc94b5143b85f9887ad
Jonas Cordes <j.cordes@hs-mannheim.de>
2026-02-27 20:58:34 +0100

    Remove SimCLR plugin and associated files from the project

--- Plugins/PluginList.cmake
+++ Plugins/PluginList.cmake
@@ -16,7 +16,6 @@ org.mitk.gui.qt.m2.docker.umap:ON
 org.mitk.gui.qt.m2.docker.molecular:ON
 org.mitk.gui.qt.m2.docker.peaklearning:ON
 
-org.mitk.gui.qt.m2.docker.simclr:OFF
 org.mitk.gui.qt.m2.docker.spatialkmeans:ON
 org.mitk.gui.qt.m2.docker.volcano:ON
 org.mitk.gui.qt.m2.docker.metabolicdepth:ON
