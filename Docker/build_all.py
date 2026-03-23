0c2fd7e8a50dbe1a4fe86319735819b3cd674e37
Jonas Cordes <j.cordes@hs-mannheim.de>
2026-03-23 10:02:44 +0100

    Remove obsolete Dockerfiles and scripts for metabolicDepth, molecular, simclr, and umap components

--- Docker/build_all.py
+++
@@ -1,9 +0,0 @@
-import os
-import glob
-import pathlib
-import sys 
-
-p = pathlib.Path(__file__).parent.joinpath("**/Dockerfile*")
-for f in glob.glob(str(p)):
-    print(f)
-    print(os.system(f"cd {pathlib.Path(f).parent} && docker build -f {f} ." ))
