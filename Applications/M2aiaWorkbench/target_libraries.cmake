840897e3a4f9e2b357c4cc912a0da325bb894074
Jonas Cordes <j.cordes@hs-mannheim.de>
2026-07-14 21:53:08 +0200

    Add feature lists plugin and remove ImzMLExport from target libraries

--- Applications/M2aiaWorkbench/target_libraries.cmake
+++ Applications/M2aiaWorkbench/target_libraries.cmake
@@ -26,8 +26,8 @@ set(target_libraries
   # org_mitk_gui_qt_m2_CombineImages
   org_mitk_gui_qt_m2_spectrum
   org_mitk_gui_qt_m2_peakpicking
-  org_mitk_gui_qt_m2_ImzMLExport
   org_mitk_gui_qt_m2_reconstruction3D
+  org_mitk_gui_qt_m2_featurelists
   org_mitk_gui_qt_m2_Position
   # org_mitk_gui_qt_m2_registration
   # org_mitk_gui_qt_matchpoint_algorithm_browser
